ec01cb4451d0a76c46214ea8561388b585a3e900
Frank <flyingfz@qq.com>
2019-08-29 14:48:05 +0800

    tmp commit . for remote dev purpose.

--- config/config.default.js
+++ config/config.default.js
@@ -1,6 +1,7 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
+const path = require("path");
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
@@ -16,11 +17,24 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1566994071635_5316';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = [ 'check' ];
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    defaultExtension: '.nj',
+    mapping: { '.nj': 'nunjucks', },
+    root: path.join(appInfo.baseDir, 'app/view')
+  };
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+    "qywx":{
+      "corp_id":"ww14b5a9d19e4f719a",
+      "contact_sync_secret": "sIrLSVSFKoBBSM4NDege8u1Vn0VydlS_D5s2G9Oi9WM"   , //企业微信 通讯录 secret
+      "agentId": "1000003" ,
+      "secret": "w5IlWWXJD8e0-mFv4PibSGInH5mCbhTaZmCeHGbb4F0"
+    }
   };
 
   return {
